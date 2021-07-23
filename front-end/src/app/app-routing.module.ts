d1c82b955c4dffaf91dabfb97e2dd82af3d10fb7
Jose Antonio <jose01.11.93@gmail.com>
2021-07-23 17:37:04 -0300

    criação do template

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/template/header/header.component';
+import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  { path: 'header', component: HeaderComponent},
+  { path: 'home' , component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/src/app/components/template/template.module.ts
+++ front-end/src/app/components/template/template.module.ts
@@ -1,20 +1,30 @@
+import { TemplateRoutingModule } from './template.routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavComponent } from './nav/nav.component';
-import { HeaderComponent } from './header/header.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
+    HeaderComponent,
     FooterComponent,
-    NavComponent,
-    HeaderComponent
+    NavComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TemplateRoutingModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    NavComponent
   ]
 })
 export class TemplateModule { }
